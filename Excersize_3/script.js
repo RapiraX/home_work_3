a35920252bf088be8af49ac8532679ac856daa6e
RapiraX <hmumuziev@gmaiil.com>
2025-01-11 09:55:20 +0600

    last changes

--- Excersize_3/script.js
+++ Excersize_3/script.js
@@ -1,24 +1,22 @@
 const userWantedArea = prompt("Enter your what area you want to learn", "");
 
-switch (userWantedArea) {
-    case "frontend":
-        console.log("You need to learn html, css, javascript, react, redux");
-        break;
-    case "backend":
-        console.log("You need to learn c#/java/python/go/php, databases, servers, network, terminal");
-        break;
-    case "devops":
-        console.log("You need to learn cloud, bash, terraform, CI\\CD");
-        break;
-    case "ux/ui":
-        console.log("You need to learn figma, sketch, photoshop, illustrator");
-        break;
-    case "qa engineer":
-        console.log("You need to learn Jira, Trello, Bugzilla, SQL, GitHub");
-        break;
-    default: 
-        console.log("You not entered wanted area");
+if (userWantedArea === "frontend") {
+    console.log("You need to learn html, css, javascript, react, redux");
 }
-
-
-
+else if (userWantedArea === "backend") {
+    console.log(
+        "You need to learn c#/java/python/go/php, databases, servers, network, terminal"
+    );
+}
+else if (userWantedArea === "devops") {
+    console.log("You need to learn cloud, bash, terraform, CI\\CD");
+}
+else if (userWantedArea === "ux/ui") {
+    console.log("You need to learn figma, sketch, photoshop, illustrator");
+}
+else if (userWantedArea === "qa engineer") {
+    console.log("You need to learn Jira, Trello, Bugzilla, SQL, GitHub");
+}
+else {
+    console.log("You not entered wanted area");
+}
